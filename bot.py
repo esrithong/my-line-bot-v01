fe6076e68db373122402a4205cf0bb4a93a010b6
Vanilla39 <43108801+Vanilla39@users.noreply.github.com>
2021-01-24 20:43:58 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,9 +42,9 @@ def handle_text_message(event):
                 TextSendMessage(text='กรุณาระบุจังหวัดและอำเภอที่ต้องการทราบ'),
             #]
        )
-    else line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    #line_bot_api.reply_message(
+        #event.reply_token,
+        #TextSendMessage(text=event.message.text))
     
    
     
