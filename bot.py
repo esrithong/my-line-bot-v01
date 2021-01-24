3e5a15ce6b11a05b3c0fe89431a8b27ecd44f280
Vanilla39 <43108801+Vanilla39@users.noreply.github.com>
2021-01-24 21:03:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,22 +21,22 @@ from linebot import (
 from linebot.exceptions import (
     LineBotApiError, InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-    SourceUser, SourceGroup, SourceRoom,
-    TemplateSendMessage, ConfirmTemplate, MessageAction,
-    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
-    PostbackAction, DatetimePickerAction,
-    CameraAction, CameraRollAction, LocationAction,
-    CarouselTemplate, CarouselColumn, PostbackEvent,
-    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
-    ImageMessage, VideoMessage, AudioMessage, FileMessage,
-    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
-    MemberJoinedEvent, MemberLeftEvent,
-    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
-    TextComponent, SpacerComponent, IconComponent, ButtonComponent,
-    SeparatorComponent, QuickReply, QuickReplyButton,
-    ImageSendMessage)
+#from linebot.models import (
+    #MessageEvent, TextMessage, TextSendMessage,
+    #SourceUser, SourceGroup, SourceRoom,
+    #TemplateSendMessage, ConfirmTemplate, MessageAction,
+    #ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
+    #PostbackAction, DatetimePickerAction,
+    #CameraAction, CameraRollAction, LocationAction,
+    #CarouselTemplate, CarouselColumn, PostbackEvent,
+    #StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    #ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    #UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
+    #MemberJoinedEvent, MemberLeftEvent,
+    #FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
+    #TextComponent, SpacerComponent, IconComponent, ButtonComponent,
+    #SeparatorComponent, QuickReply, QuickReplyButton,
+    #ImageSendMessage)
 
 app = Flask(__name__)
 # line_bot_api = Channel access token
