226b37bb8ed7b7c7e2aeff22145ffca054fa3fdd
Vanilla39 <43108801+Vanilla39@users.noreply.github.com>
2021-01-30 21:12:46 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,14 +39,14 @@ def handle_text_message(event):
     
    text = event.message.text
     
-   if text == 'พยากรณ์อากาศ':
+   if text == 'พยากรณ์อากาศ':"""
        #quota = line_bot_api.get_message_quota()
        line_bot_api.reply_message(
             event.reply_token, [
                 TextSendMessage(text='กรุณาระบุจังหวัดและอำเภอที่ต้องการทราบ'),
             ]
        )
-"""   
+   
     elif text == 'นครปฐม เมืองนครปฐม':
         #quota = line_bot_api.get_message_quota()
         #import requests
