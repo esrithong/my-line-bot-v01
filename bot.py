b9a2dde8d138edc736d86c8ec26d35bdc89d94da
Vanilla39 <43108801+Vanilla39@users.noreply.github.com>
2021-05-29 15:26:42 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,32 @@ from flask import request
 from flask import make_response
 
 
-# Flask
 app = Flask(__name__)
-@app.route('/', methods=['POST']) #Using post as a method
-
+# line_bot_api = Channel access token
+# handler = Channel secret
 line_bot_api = LineBotApi('4OPjwBiZQWRlAmGTII1pdeLdygBdE7fBW9hkdsZ8t3SaJ/fnkCYEnJzC24mD0bBZtdEAM5UxZ7egUEAf7gjmQHLUUz8Lv4cjGwC3OZ/vAJmuXW1YYP/yvNo08cgrC0ZqOHB5aB0QC2n3g6DiCZFtkgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('501aad4d05afc7da48e8c5400c2f61e1')
 
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
 def MainFunction():
 
     #Getting intent from Dailogflow
@@ -74,8 +93,5 @@ def BMI_calculation(respond_dict): #Function for calculating BMI
         answer_function = "คุณอ้วนมาก"
     return answer_function
 
-#Flask
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    print("Starting app on port %d" % port)
-    app.run(debug=False, port=port, host='0.0.0.0', threaded=True)
+if __name__ == "__main__":
+    app.run()
