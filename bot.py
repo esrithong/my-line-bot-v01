b82085352f7541e7c514673f4d675f4e1a2d65c4
Vanilla39 <43108801+Vanilla39@users.noreply.github.com>
2021-08-10 21:27:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,97 +1,73 @@
 import json
 import os
+
 from flask import Flask
 from flask import request
 from flask import make_response
 
+import linebot
+from linebot.models import *
+from linebot import *
+from linebot import (LineBotApi, WebhookHandler)
 
-app = Flask(__name__)
 # line_bot_api = Channel access token
 # handler = Channel secret
-line_bot_api = LineBotApi('4OPjwBiZQWRlAmGTII1pdeLdygBdE7fBW9hkdsZ8t3SaJ/fnkCYEnJzC24mD0bBZtdEAM5UxZ7egUEAf7gjmQHLUUz8Lv4cjGwC3OZ/vAJmuXW1YYP/yvNo08cgrC0ZqOHB5aB0QC2n3g6DiCZFtkgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('501aad4d05afc7da48e8c5400c2f61e1')
+line_bot_api = LineBotApi('ZdDlzPzU2/VBLtW7GCdJq9VFW61tDUXtTKnLJdSLgswx1oS0FODSgBSxpvdIASb2WhmetKizcWP7Rm7VwsGudUgZ8w69PuspqMP3Elauwuz5M4IEqnYCgKDQ1f3UD8fvNnMVlaW3mf8aRJU/nGnTkAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('cb489aedab482e519846c563f4cad0ca')
+
+app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/webhook", methods=['POST'])
 def webhook():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-def MainFunction():
-
-    #Getting intent from Dailogflow
-    question_from_dailogflow_raw = request.get_json(silent=True, force=True)
-
-    #Call generating_answer function to classify the question
-    answer_from_bot = generating_answer(question_from_dailogflow_raw)
-    
-    #Make a respond back to Dailogflow
-    r = make_response(answer_from_bot)
-    r.headers['Content-Type'] = 'application/json' #Setting Content Type
-
+    req = request.get_json(silent=True, force=True)
+    res = processRequest(req)
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
     return r
 
-def generating_answer(question_from_dailogflow_dict):
+def processRequest(req):
 
-    #Print intent that recived from dialogflow.
-    print(json.dumps(question_from_dailogflow_dict, indent=4 ,ensure_ascii=False))
+    # Parsing the POST request body into a dictionary for easy access.
+    req_dict = json.loads(request.data)
 
-    #Getting intent name form intent that recived from dialogflow.
-    intent_group_question_str = question_from_dailogflow_dict["queryResult"]["intent"]["displayName"] 
+    # Accessing the fields on the POST request boduy of API.ai invocation of the webhook
+    intent = req_dict["queryResult"]["intent"]["displayName"]
 
-    #Select function for answering question
-    if intent_group_question_str == 'กินอะไรดี':
-        answer_str = menu_recormentation()
-    elif intent_group_question_str == 'BMI - Confirmed W and H': 
-        answer_str = BMI_calculation(question_from_dailogflow_dict)
-    else: answer_str = "ผมไม่เข้าใจ คุณต้องการอะไร"
+    if intent == 'A_Test':
+        speech = 'ทดสอบสำเร็จ'
+        '''
+        doc_ref = db.collection(u'Nogizaka46').document(u'A_Test')
+        doc = doc_ref.get().to_dict()
+        print(doc)
 
-    #Build answer dict 
-    answer_from_bot = {"fulfillmentText": answer_str}
-    
-    #Convert dict to JSON
-    answer_from_bot = json.dumps(answer_from_bot, indent=4) 
-    
-    return answer_from_bot
+        engName = doc['EngName']
+        thName = doc['ThName']
+        speech = f'ชื่อภาษาอังกฤษ คือ  {engName} ชื่อภาษาไทย คือ {thName} ทดสอบสำเร็จ'
+        '''
 
-def menu_recormentation(): #Function for recommending menu
-    menu_name = 'สุกี้แห้ง'
-    answer_function = menu_name + ' สิ น่ากินนะ'
-    return answer_function
+    else:
 
-def BMI_calculation(respond_dict): #Function for calculating BMI
+        speech = "ทดสอบไม่สำเร็จ"
 
-    #Getting Weight and Height
-    weight_kg = float(respond_dict["queryResult"]["outputContexts"][2]["parameters"]["Weight.original"])
-    height_cm = float(respond_dict["queryResult"]["outputContexts"][2]["parameters"]["Height.original"])
-    
-    #Calculating BMI
-    BMI = weight_kg/(height_cm/100)**2
-    if BMI < 18.5 :
-        answer_function = "คุณผอมเกินไปนะ"
-    elif 18.5 <= BMI < 23.0:
-        answer_function = "คุณมีนำ้หนักปกติ"
-    elif 23.0 <= BMI < 25.0:
-        answer_function = "คุณมีนำ้หนักเกิน"
-    elif 25.0 <= BMI < 30:
-        answer_function = "คุณอ้วน"
-    else :
-        answer_function = "คุณอ้วนมาก"
-    return answer_function
+    res = makeWebhookResult(speech)
 
-if __name__ == "__main__":
-    app.run()
+    return res
+
+def makeWebhookResult(speech):
+
+    return {
+  "fulfillmentText": speech
+    }
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0', threaded=True)
